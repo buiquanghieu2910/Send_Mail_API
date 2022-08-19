c02b91f2e17da4f667474e62c3fba4dc054c4502
hieubqph13812 <bui.quang.hieu2910@gmail.com>
2022-08-20 01:32:35 +0700

    hieubq update 1h32p 20/08/22

--- TestSendMail/src/main/java/com/hieubq/UploadImage/UploadImageRestController.java
+++ TestSendMail/src/main/java/com/hieubq/UploadImage/UploadImageRestController.java
@@ -1,18 +1,23 @@
 package com.hieubq.UploadImage;
 
 import java.io.File;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Logger;
 
 import javax.servlet.ServletContext;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -26,6 +31,8 @@ import com.hieubq.Service.UploadService;
 @CrossOrigin("*")
 @RestController
 public class UploadImageRestController {
+	
+	private static final Logger logger = Logger.getLogger(UploadImageRestController.class.getName());
 	@Autowired
 	UploadService uploadService;
 
@@ -37,6 +44,63 @@ public class UploadImageRestController {
 		node.put("name", savedFile.getName());
 		node.put("size", savedFile.length());
 		return node;
+	}
+	
+	
+	@PostMapping("/rest/upload/images/lst")
+	public JsonNode uploadLst(@PathVariable("file") MultipartFile[] files) {
+//		File savedFile = uploadService.save(file, folder);
+		ObjectMapper mapper = new ObjectMapper();
+		ObjectNode node = mapper.createObjectNode();
+//		node.put("name", savedFile.getName());
+//		node.put("size", savedFile.length());
+
+//			 if (files[i].isFile()) {
+//				    System.out.println("File " + file[i].getName());
+//				  } else if (files[i].isDirectory()) {
+//				    System.out.println("Directory " + files[i].getName());
+//				  }
+			if (files == null || files.length == 0) {
+				throw new RuntimeException("You must select at least one file for uploading");
+			}
+
+			StringBuilder sb = new StringBuilder(files.length);
+			for (int i = 0; i < files.length; i++) {
+				System.out.println(files[i].getName());
+			}
+		
+		return node;
+	}
+	
+	@PostMapping("/upload/multiple/files")
+	public ResponseEntity<JsonNode> uploadData(@RequestParam("multipleFiles") MultipartFile[] files) throws Exception {
+		ObjectMapper mapper = new ObjectMapper();
+		ObjectNode node = mapper.createObjectNode();
+		if (files == null || files.length == 0) {
+			throw new RuntimeException("You must select at least one file for uploading");
+		}
+
+		StringBuilder sb = new StringBuilder(files.length);
+
+		for (int i = 0; i < files.length; i++) {
+			InputStream inputStream = files[i].getInputStream();
+			String originalName = files[i].getOriginalFilename();
+			String name = files[i].getName();
+			String contentType = files[i].getContentType();
+			long size = files[i].getSize();
+
+			sb.append("File Name: " + originalName + "\n");
+
+			logger.info("InputStream: " + inputStream);
+			logger.info("OriginalName: " + originalName);
+			logger.info("Name: " + name);
+			logger.info("ContentType: " + contentType);
+			logger.info("Size: " + size);
+		}
+
+		// Do processing with uploaded file data in Service layer
+//		return new ResponseEntity<String>(sb.toString(), HttpStatus.OK);
+		return ResponseEntity.ok(node);
 	}
 
 	@GetMapping("/get-all-folder")
